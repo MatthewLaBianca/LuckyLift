2e2405e0eff1cb403d302267451a27b3134d2514
MatthewLaBianca <Matthew.Labianca@IGT.com>
2015-12-02 12:38:22 -0500

    bug fixes

--- LuckyLift/Assets/prototype/Scripts/ListManager.cs
+++ LuckyLift/Assets/prototype/Scripts/ListManager.cs
@@ -7,11 +7,11 @@ using System.Collections.Generic;
 public class ListManager : MonoBehaviour {
 
     public List<int> floors = new List<int>();
-    public List<int> selectedFloors = new List<int>();
+    public List<int> selectedFloors = new List<int>();		//this can turn private once it is ready to publish
 
     public int correctFloors;
     public Text winOrLose;
-	// Use this for initialization
+
 	void Start () {
         correctFloors = 0;  //Initialize the game so that at the beginning there are no correct floors
         for (int i = 0; i < floors.Count; i++)              //Randomize the list of floors
@@ -26,20 +26,19 @@ public class ListManager : MonoBehaviour {
 
     public void AddFloorToSelectedFloors(int chosenFloor)   //When a button is selected,
     {                                                       //make a new list of selected floors
-        selectedFloors.Add(floors[chosenFloor - 1]);
+        selectedFloors.Add(floors[chosenFloor - 1]);		//on button press, pass in the chosen floor so that we know how many winning floors have been chosen
     }
 
-
-    //This is for testing
+	
     public void SubmitTicket()
     {
-        foreach(int floor in selectedFloors)
-        {
-            if (floor == 1)
+        foreach(int floor in selectedFloors)		//check each "floor" in the list.
+        {											//if the floor is a 1, it is a winning floor
+            if (floor == 1)							//if the floor holds a 0, it is a losing floor
             {
                 correctFloors++;
             }
         }
-        //winOrLose.text = correctFloors.ToString() + " Correct Floors!";
+        //winOrLose.text = correctFloors.ToString() + " Correct Floors!";	//this was for testing
     }
 }
