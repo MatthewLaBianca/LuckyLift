449157cbef499752f029cbab431a0a53b66159e6
MatthewLaBianca <Matthew.Labianca@IGT.com>
2015-12-15 13:58:25 -0500

    menu switched to lerping

--- LuckyLift/Assets/Scripts/MenuAnim.cs
+++ LuckyLift/Assets/Scripts/MenuAnim.cs
@@ -3,22 +3,102 @@ using System.Collections;
 
 public class MenuAnim : MonoBehaviour {
 
-	public Animation slide1;
-	public AnimationClip slideclip1;
+	public GameObject menu;
+	public GameObject target;
+	public GameObject target1back;
+	public GameObject target2;
+	public GameObject targetFinalBack;
 
 	// Use this for initialization
 	void Start () {
-		slideclip1.legacy = true;
-		slide1.AddClip(slideclip1,"slide1");
+
 	}	
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+	}
+		
+	public void SlideToSecond()
+	{
+		StartCoroutine(lerp1(1.3f));
 	}
 
-	public void PlayFirstSlide()
+	public void SlideToFirst()
 	{
-		slide1.Play("slide1");
+		StartCoroutine(lerp2(1.3f));
+	}
+
+	public void SlideToLast()
+	{
+		StartCoroutine(lerp3(1.3f));
+	}
+	public void SlideTo2()
+	{
+		StartCoroutine(lerp4(1.3f));
+	}
+
+	IEnumerator slideToTheLeft(float overTime)
+	{
+		float startTime = Time.time;
+		while(Time.time <= startTime + overTime)
+		{
+			menu.transform.position += new Vector3(-15,0,0);
+			yield return null;
+		}
+
+	}
+
+	IEnumerator lerp1(float overTime)
+	{
+		float startTime = Time.time;
+		while(Time.time <= startTime + overTime)
+		{
+			menu.transform.position = Vector2.Lerp(this.transform.position,target.transform.position,Time.deltaTime);
+			yield return null;
+		}
+
+	}
+	IEnumerator lerp2(float overTime)
+	{
+		float startTime = Time.time;
+		while(Time.time <= startTime + overTime)
+		{
+			menu.transform.position = Vector2.Lerp(this.transform.position,target1back.transform.position,Time.deltaTime);
+			yield return null;
+		}
+
+	}
+	IEnumerator lerp3(float overTime)
+	{
+		float startTime = Time.time;
+		while(Time.time <= startTime + overTime)
+		{
+			menu.transform.position = Vector2.Lerp(this.transform.position,target2.transform.position,Time.deltaTime);
+			yield return null;
+		}
+
+	}
+
+	IEnumerator lerp4(float overTime)
+	{
+		float startTime = Time.time;
+		while(Time.time <= startTime + overTime)
+		{
+			menu.transform.position = Vector2.Lerp(this.transform.position,targetFinalBack.transform.position,Time.deltaTime);
+			yield return null;
+		}
+
+	}
+
+	IEnumerator slideToTheRight(float overTime)
+	{
+		float startTime = Time.time;
+		while(Time.time <= startTime + overTime)
+		{
+			menu.transform.position += new Vector3(15,0,0);
+			yield return null;
+		}
+
 	}
 }
