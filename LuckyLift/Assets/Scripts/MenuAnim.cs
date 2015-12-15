cf91d67b3fab233bcd9be70529dd69b7eaefca51
MatthewLaBianca <Matthew.Labianca@IGT.com>
2015-12-15 13:01:11 -0500

    first animation!!!!!!

---
+++ LuckyLift/Assets/Scripts/MenuAnim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuAnim : MonoBehaviour {
+
+	public Animation slide1;
+	public AnimationClip slideclip1;
+
+	// Use this for initialization
+	void Start () {
+		slideclip1.legacy = true;
+		slide1.AddClip(slideclip1,"slide1");
+	}	
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlayFirstSlide()
+	{
+		slide1.Play("slide1");
+	}
+}
